baa62579afc69911de4ee750faa6ca11754ca3d0
jfparrado <jnsparrado@hotmail.com>
2023-04-19 16:33:29 -0500

    notfound added

--- components/TemplateManyMovies.vue
+++ components/TemplateManyMovies.vue
@@ -1,40 +1,63 @@
 <template>
-    <div>
-      <br />
-      <h2>{{titulo}}</h2>
-      <template v-if="movies.length !== 0">
-        <template v-for="index in [0, 1, 2, 3]">
-          <section class="image-section">
-            <template v-for="video in movies.slice(index * 5, (index + 1) * 5)">
-              <div class="image-container">
-                <nuxt-link :to="'/detailmovie/' + video.id">
-                  <img :src="'https://image.tmdb.org/t/p/w185/' + video.poster_path" :alt="'poster of ' + video.original_title"/>
-                  <p class="image-text">{{ video.original_title }}</p>
-                </nuxt-link>
-              </div>
-            </template>
-          </section>
-        </template>
+  <div>
+    <br />
+    <h2>{{titulo}}</h2>
+    <template v-if="movies.length !== 0">
+      <template v-for="index in [0, 1, 2, 3]">
+        <section class="image-section">
+          <template v-for="video in movies.slice(index * 5, (index + 1) * 5)">
+            <div class="image-container">
+              <nuxt-link :to="'/detailmovie/' + video.id">
+                <img :src="'https://image.tmdb.org/t/p/w185/' + video.poster_path" :alt="'poster of ' + video.original_title"/>
+                <p class="image-text">{{ video.original_title }}</p>
+              </nuxt-link>
+            </div>
+          </template>
+        </section>
       </template>
-      <template v-else>
-        <div class="imgContainer">
-          <img src="~/assets/loading.gif" alt="loading" />
-        </div>
-      </template>
-    </div>
-  </template>
+      <slot name="loading"><img src="~/assets/loading.gif" alt="loading" /></slot>
+    </template>
+    <template v-else>
+      <slot name="no-results"><p id="noResults">No se encontraron resultados</p></slot>
+    </template>
+  </div>
+</template>
+
   
 
 <script>
- export default {
-    props: {
-      titulo: String,
-      movies: Array
-    }
+export default {
+  props: {
+    titulo: String,
+    movies: Array
+  },
+  data() {
+    return {
+      showNoResults: false
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      if (this.movies.length === 0) {
+        this.showNoResults = true;
+      }
+    }, 3000);
   }
+};
 </script>
 
 <style scoped>
+
+  #noResults{
+    font-size: 3rem;
+    color: white;
+    text-shadow: 1px 1px 2px black;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
 img {
     max-width: 100%;
     max-height: 300px;
