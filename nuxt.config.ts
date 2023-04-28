e689e507cd50e5b3bd0d7d2f3cdc70aee42a82e3
jfparrado <jnsparrado@hotmail.com>
2023-04-28 10:00:59 -0500

    todo funcional

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-    css: [
-        '~/assets/css/global.css'
-      ]
-})
+export default {
+  css: [
+    '~/assets/css/global.css'
+  ],
+  modules: [
+    '@nuxtjs/firebase',
+    {
+      config: {
+        apiKey: "...",
+        authDomain: "...",
+        projectId: "...",
+        storageBucket: "...",
+        messagingSenderId: "...",
+        appId: "...",
+        measurementId: "..."
+      },
+      services: {
+        auth: true,
+        firestore: true,
+        storage: true
+      }
+    }
+  ]
+}
